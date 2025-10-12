d682a8ebc884495a3119700c0e93297109679393
OliverWilliamWalker <oliver6596@live.co.uk>
2025-10-12 19:36:32 +0100

    Under the base first pass

---
+++ level-design-test/Assets/Rotate (1).cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed;
+    public bool xAxis;
+    public bool yAxis;
+    public bool zAxis;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (xAxis == true)
+        {
+            transform.Rotate(speed, 0, 0, Space.Self);
+        }
+        if (yAxis == true)
+        {
+            transform.Rotate( 0, speed, 0, Space.Self);
+        }
+        if (zAxis == true)
+        {
+            transform.Rotate(0, 0, speed, Space.Self);
+        }
+
+    }
+}
